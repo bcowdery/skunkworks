94d22025cdc491def0155cb0692010c104bf53bf
Brian Cowdery <brian@symend.com>
2020-10-30 10:46:12 -0600

    Minor heartbeat tweaks

--- sources/portauthority/src/PortAuthority/HealthChecks/HeartbeatMonitor.cs
+++ sources/portauthority/src/PortAuthority/HealthChecks/HeartbeatMonitor.cs
@@ -96,32 +96,4 @@ namespace PortAuthority.HealthChecks
             _heartbeats.Remove(sourceAddress, out _);
         }
     }
-    
-    /// <summary>
-    /// Heartbeat status
-    /// </summary>
-    public struct HeartbeatStatus
-    {
-        public HeartbeatStatus(Uri sourceAddress, TimeSpan age, bool isAlive)
-        {
-            SourceAddress = sourceAddress;
-            Age = age;
-            IsAlive = isAlive;
-        }
-
-        /// <summary>
-        /// Source address of the heartbeat
-        /// </summary>
-        public Uri SourceAddress { get; }
-        
-        /// <summary>
-        /// Age of the entry, how long ago was the last heartbeat recorded.
-        /// </summary>
-        public TimeSpan Age { get; }
-        
-        /// <summary>
-        /// True if endpoint is alive, false if heartbeat is outside of the configured threshold
-        /// </summary>
-        public bool IsAlive { get; }
-    }
 }
--- sources/portauthority/src/PortAuthority/HealthChecks/IHeartbeatMonitor.cs
+++ sources/portauthority/src/PortAuthority/HealthChecks/IHeartbeatMonitor.cs
@@ -12,4 +12,32 @@ namespace PortAuthority.HealthChecks
         void AddHeartbeat(Uri sourceAddress, DateTimeOffset heartbeat);
         void RemoveHeartbeat(Uri sourceAddress);
     }
+    
+    /// <summary>
+    /// Heartbeat status
+    /// </summary>
+    public readonly struct HeartbeatStatus
+    {
+        public HeartbeatStatus(Uri sourceAddress, TimeSpan age, bool isAlive)
+        {
+            SourceAddress = sourceAddress;
+            Age = age;
+            IsAlive = isAlive;
+        }
+
+        /// <summary>
+        /// Source address of the heartbeat
+        /// </summary>
+        public Uri SourceAddress { get; }
+        
+        /// <summary>
+        /// Age of the entry, how long ago was the last heartbeat recorded.
+        /// </summary>
+        public TimeSpan Age { get; }
+        
+        /// <summary>
+        /// True if endpoint is alive, false if heartbeat is outside of the configured threshold
+        /// </summary>
+        public bool IsAlive { get; }
+    }    
 }
